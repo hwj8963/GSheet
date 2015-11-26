057fe35ed76fc18861a9cb9940339edc9aebcfa1
bms <lastsonic@gmail.com>
2015-11-26 19:40:37 +0900

    add unity project

---
+++ GSheet/Assets/Editor/GSheetSettings.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class GSheetSettings : ScriptableObject {
+
+
+
+	public string CLIENT_ID;
+	public string CLIENT_SECRET;
+
+
+	static readonly string SCOPE = "https://spreadsheets.google.com/feeds https://docs.google.com/feeds";
+	static readonly string REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	[MenuItem("Custom/GSheet/Create GSheet Settings")]
+	public static void CreateSettings() {
+		var setting = AssetDatabase.FindAssets ("t:GSheetSettings");
+		if (setting.Length > 0) {
+			Debug.Log ("setting already exist : " + AssetDatabase.GUIDToAssetPath(setting[0]));
+		} else {
+			GSheetSettings asset = ScriptableObject.CreateInstance<GSheetSettings> ();
+			
+			string path = AssetDatabase.GetAssetPath (Selection.activeObject);
+			if (path == "") 
+			{
+				path = "Assets";
+			} 
+			else if (Path.GetExtension (path) != "") 
+			{
+				path = path.Replace (Path.GetFileName (AssetDatabase.GetAssetPath (Selection.activeObject)), "");
+			}
+			
+			string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath (path + "/CreateGSheetSettings.asset");
+			
+			AssetDatabase.CreateAsset (asset, assetPathAndName);
+			
+			AssetDatabase.SaveAssets ();
+			AssetDatabase.Refresh();
+			EditorUtility.FocusProjectWindow ();
+			Selection.activeObject = asset;
+		}
+
+
+	}
+}
---
+++ GSheet/Assets/Editor/GSheetsettingsEditor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GSheetSettings))]
+public class GSheetsettingsEditor : Editor {
+
+	public override void OnInspectorGUI ()
+	{
+		base.OnInspectorGUI ();
+	}
+}
